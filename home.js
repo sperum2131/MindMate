a5518955e79f9f3ad95def7d1403450f61f655dc
Siddharth Perumalla <siddharthperumalla@mac.lan>
2025-08-01 22:52:00 -0400

    save to localstorage

--- home.js
+++ home.js
@@ -1,12 +1,42 @@
 const updateButtons = document.querySelectorAll('.form-item button');
+const formInputs = document.querySelectorAll('.form-item input');
 
 updateButtons.forEach(button => {
     button.addEventListener('click', () => {
         const input = button.previousElementSibling;
-        input.readOnly = false;
-        input.focus();
-        input.addEventListener('blur', () => {
-            input.readOnly = true;
-        });
+        
+        if (button.textContent.includes('Update')) {
+            button.textContent = 'Save ' + button.previousElementSibling.id.charAt(0).toUpperCase() + button.previousElementSibling.id.slice(1);
+            input.readOnly = false;
+            input.focus();
+            input.classList.remove('incorrect-input');
+        } else {
+            if (input.value >= 1 && input.value <= 10) {
+                localStorage.setItem(input.id, input.value);
+                input.readOnly = true;
+                button.textContent = 'Update ' + button.previousElementSibling.id.charAt(0).toUpperCase() + button.previousElementSibling.id.slice(1);
+                input.classList.remove('incorrect-input');
+            } else {
+                input.classList.add('incorrect-input');
+                input.focus();
+            }
+        }
     });
+});
+
+formInputs.forEach(input => {
+    input.addEventListener('input', () => {
+        input.classList.remove('incorrect-input');
+    });
+});
+
+function loadFromLocalStorage(input) {
+    const value = localStorage.getItem(input.id);
+    if (value) {
+        input.value = value;
+    }
+}
+
+formInputs.forEach(input => {
+    loadFromLocalStorage(input);
 });
